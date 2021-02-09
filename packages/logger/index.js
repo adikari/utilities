b6923d158fdcaffe2cfcc0131399c9ec5b285be0
subash <me@subash.com.au>
2021-02-09 15:04:15 +1100

    feat: add logger

---
+++ packages/logger/index.js
@@ -0,0 +1,27 @@
+const pino = require('pino');
+const { v4: uuid } = require('uuid');
+
+const metadata = {
+  correlationId: uuid(),
+  stage: process.env.STAGE,
+  origin: process.env.SERVICE_NAME,
+  awsRegion: process.env.AWS_REGION || process.env.AWS_DEFAULT_REGION,
+  functionName: process.env.AWS_LAMBDA_FUNCTION_NAME,
+  functionVersion: process.env.AWS_LAMBDA_FUNCTION_VERSION,
+  functionMemorySize: process.env.AWS_LAMBDA_FUNCTION_MEMORY_SIZE
+};
+
+const logger = pino({
+  level: process.env.LOG_LEVEL || (metadata.stage === 'dev' ? 'debug' : 'info'),
+  redact: ['address', 'phone', 'email'],
+  mixin: () => ({ ...metadata })
+});
+
+const setCorrelationId = id => {
+  metadata.correlationId = id;
+};
+
+module.exports = {
+  setCorrelationId,
+  logger
+};
